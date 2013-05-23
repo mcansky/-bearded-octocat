697c9a1fb6b6afd3a0f11138e868465aae9a4403
Thomas Riboulet <riboulet@gmail.com>
2013-05-23 16:47:14 +0200

    adding rails generated controller
    basic route, basic return

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe PhotosController do
   describe "index" do
-    it "should give a 200" do
-      get :index
-      response.status.should eq 200
+    it "should exist as action" do
+      expect { get :index }.to_not raise_error AbstractController::ActionNotFound
     end
   end
 end

b97bf1cf4eb1903c139d85f1b9f5162e64bd2577
Thomas Riboulet <riboulet@gmail.com>
2013-05-23 16:38:21 +0200

    adding required methods skeletons

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,13 @@
 class Photo < ActiveRecord::Base
   attr_accessible :desc, :image, :score
   mount_uploader :image, ImageUploader
+
+  def url
+  end
+
+  def age
+  end
+
+  def score_up
+  end
 end

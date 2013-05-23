c4bd6d81b23efed78ca82a3f6b96017861302aca
Thomas Riboulet <riboulet@gmail.com>
2013-05-23 16:49:46 +0200

    adding placeholder array for @photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
   def index
+    @photos = []
   end
 end
--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -8,5 +8,17 @@ describe PhotosController do
     it "should have its template" do
       expect { get :index }.to_not raise_error ActionView::MissingTemplate
     end
+    it "should return a 200" do
+      get :index
+      response.status.should eq 200
+    end
+    it "should have @photos object" do
+      get :index
+      assigns(:photos).should_not eq nil
+    end
+    it "should have @photos array" do
+      get :index
+      assigns(:photos).class.should eq Array
+    end
   end
 end

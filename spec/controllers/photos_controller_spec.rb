e043a1da2a368f6275a061621c054a6c8c672615
Thomas Riboulet <riboulet@gmail.com>
2013-05-23 16:47:55 +0200

    adding template

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -5,5 +5,8 @@ describe PhotosController do
     it "should exist as action" do
       expect { get :index }.to_not raise_error AbstractController::ActionNotFound
     end
+    it "should have its template" do
+      expect { get :index }.to_not raise_error ActionView::MissingTemplate
+    end
   end
 end

20484169484e17dc8d2cc573cec80059dac40b05
Thomas Riboulet <riboulet@gmail.com>
2013-05-23 16:22:37 +0200

    adding rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VarageSale::Application.config.secret_token = '423c5d30421f560cf7eb49842727bf09a602fbdb33a57553e6121f91c4646e14774d3f1c79d9ecf5491ecc12fd42fa5321002c357911e4c8ea00a8449d92bdf3'
